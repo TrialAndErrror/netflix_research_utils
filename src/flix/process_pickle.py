270223649c8e00d6f0a664bcf9928e959de5099d
TrialAndErrror <coding@trialanderrror.com>
2022-01-02 17:38:51 -0500

    fixing processing, excluding non-pickle files, accounting for errors.

--- src/flix/process_pickle.py
+++ src/flix/process_pickle.py
@@ -6,13 +6,18 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
-from debug_messages import print_green
+import glob
+from debug_messages import print_green, print_red
 
 
 def check_for_missing_data(obj):
     soup = BeautifulSoup(obj, "lxml")
-    content_div = soup.find_all('div', {'class': 'content'})[-1]
-    return not re.search('No streaming data', content_div)
+    try:
+        content_div = soup.find_all('div', {'class': 'content'})[-1]
+    except IndexError:
+        return True
+    else:
+        return re.search('No streaming data', content_div.text)
 
 
 def read_soup(filename):
@@ -20,7 +25,12 @@ def read_soup(filename):
     df = None
     obj = load_pickle(Path(PICKLE_DIR, filename))
 
-    missing_data = check_for_missing_data(obj)
+    try:
+        missing_data = check_for_missing_data(obj)
+    except TypeError:
+        print_red('Error: Object text might not exist')
+        print(f'Corrupted text: {obj}')
+        missing_data = True
 
     if not missing_data:
         df = pd.read_html(obj)[-1]
@@ -29,7 +39,8 @@ def read_soup(filename):
 
 
 def make_dfs():
-    files = os.listdir(PICKLE_DIR)
+    # files = os.listdir(PICKLE_DIR)
+    files = glob.glob(f'{PICKLE_DIR}/*.pickle')
     files_count = len(files)
     counter = 1
 
@@ -38,7 +49,7 @@ def make_dfs():
     for file in files:
         print(f'Working on {counter}/{files_count}')
         title, df = read_soup(file)
-        if df:
+        if isinstance(df, pd.DataFrame):
             print_green(f'Found top 10 data for {title}')
             df_dict[title] = df
 
@@ -48,8 +59,11 @@ def make_dfs():
     print('\n\nResults saved.\n\n')
 
 
-
-if __name__ == '__main__':
+def test_debug():
     top_10_filename = 'red-notice.pickle'
     no_results_filename = 'quincy.pickle'
     read_soup(no_results_filename)
+
+
+if __name__ == '__main__':
+    make_dfs()
