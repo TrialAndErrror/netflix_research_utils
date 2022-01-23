f1b91bd0c09c38dbff4a9ec8662efc212e59ad2d
TrialAndErrror <coding@trialanderrror.com>
2022-01-23 16:29:54 -0500

    updating farmer to export reuslts in a more clean fashion

--- src/pickle_farm/farmer.py
+++ src/pickle_farm/farmer.py
@@ -22,13 +22,30 @@ def run_all_pickles(pickle_dir: str = PICKLES_DIR):
             error_list.append(str(pickle).split('/')[-1].split('.')[-2])
 
     df = pd.DataFrame.from_records(all_entries)
-    df.to_csv('final_df.csv')
+
+    results_dir = Path(os.getcwd(), 'results')
+    results_dir.mkdir(exist_ok=True)
+
+    output_folder = Path(results_dir, f'Results {datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}')
+    output_folder.mkdir(exist_ok=True)
+    final_df_path = Path(output_folder, 'final_df.csv')
+    print(f'Saving Overall Dataframe to {final_df_path}')
+    df.to_csv(str(final_df_path))
+
+    countries = list(df['Country'].unique())
+    countries_dir = Path(output_folder, 'country_dfs')
+    countries_dir.mkdir(exist_ok=True)
+    for country in countries:
+        print(f'Saving Country Dataframe: {country}')
+        country_df = df[df['Country'] == country]
+        country_df_path = Path(countries_dir, f'{country}.csv')
+        country_df.to_csv(str(country_df_path))
 
     print('List of Errors:')
     [print(item) for item in error_list]
 
     now = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
-    error_file = Path(os.getcwd(), f'error_list {now}.txt')
+    error_file = Path(output_folder, f'error_list {now}.txt')
     error_file.write_text('\n'.join(error_list))
 
 
