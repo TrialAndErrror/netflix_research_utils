e1552d493378c2e993b8f0a9ea9756c5b0de2eef
TrialAndErrror <coding@trialanderrror.com>
2022-01-30 11:58:47 -0500

    Adding indexing for countries and filenames

--- notebook_utils/anova_interactive.py
+++ notebook_utils/anova_interactive.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from notebook_utils.anova_models import AnovaAnalysis
 import pandas as pd
@@ -20,19 +21,30 @@ def get_anova_loop() -> str:
                 print(f'Using ({result}) as ANOVA model\n')
                 return result
             else:
-                print('Error: invalid syntax. Please include a ~ in your ANOVA formula or choose a number.')
+                print('Error: invalid syntax. Please include a ~ in your ANOVA formula or choose a number.\n')
         else:
             print(f'Using ({anova_model}) as ANOVA model\n')
             return anova_model
 
 
 def get_df_file_loop() -> Path:
-        df_loop = True
-        while df_loop:
+    df_loop = True
+    while df_loop:
+        csv_files = [item for item in os.listdir(os.getcwd()) if item.endswith('.csv')]
+        files = {str(index + 1): file for index, file in enumerate(csv_files)}
+        if files:
+            [print(f'{key}: {value}') for key, value in files.items()]
+            df_path = input('Choose Number or Enter filename/path for Dataframe CSV File: ')
+        else:
             df_path = input('Enter filename/path for Dataframe CSV File: ')
+        if files.get(df_path):
+            result = Path(files.get(df_path))
+            print(f'Loading {result.absolute()}\n')
+            return result
+        else:
             result = Path(df_path)
             if not result.exists():
-                print('Error: File not found. Please enter a valid filename or path to csv file.')
+                print('Error: File not found. Please enter a valid filename or path to csv file.\n')
             else:
                 print(f'Loading {result.absolute()}\n')
                 return result
@@ -42,19 +54,27 @@ def get_country_loop(df: pd.DataFrame) -> str:
     countries = list(df.country.unique())
     df_loop = True
     while df_loop:
-        print(f'Countries available: {countries}')
-        country = input('Enter country to analyze: ')
-        if country not in countries:
-            country = country.capitalize()
+        print(f'Countries available:')
+        country_dict = {str(value + 1): name for value, name in enumerate(countries)}
+        [print(f'{key}: {value}') for key, value in country_dict.items()]
+        country = input('Choose Number or Enter country to analyze: ')
+
+        dict_result = country_dict.get(country)
+        if dict_result:
+            print(f'Using {dict_result} for analysis.\n')
+            return dict_result
+        else:
             if country not in countries:
-                print(f'Error: Country {country} not found in {countries}.'
-                      f'\nPlease choose a valid country name, including capitalization')
+                country = country.capitalize()
+                if country not in countries:
+                    print(f'Error: Country {country} not found in {countries}.'
+                          f'\nPlease choose a valid country name, including capitalization\n')
+                else:
+                    print(f'Using {country} for analysis.\n')
+                    return country
             else:
                 print(f'Using {country} for analysis.\n')
                 return country
-        else:
-            print(f'Using {country} for analysis.\n')
-            return country
 
 
 def anova_interactive():
