219f34dce9a56391aecba475761417455b799c2e
TrialAndErrror <coding@trialanderrror.com>
2022-03-03 22:44:22 -0500

    Upodating anova automation

---
+++ notebook_utils/ANOVA/anova_main.py
@@ -0,0 +1,50 @@
+import pickle
+
+from notebook_utils.ANOVA.anova_models import AnovaAnalysis
+import pandas as pd
+from pathlib import Path
+import os
+
+
+def anova_main():
+    input_folder = Path(os.getcwd(), 'input')
+    input_folder.mkdir(exist_ok=True)
+
+    output_folder = Path(os.getcwd(), 'output')
+    output_folder.mkdir(exist_ok=True)
+
+    image_out_path = Path(os.getcwd(), 'images')
+    image_out_path.mkdir(exist_ok=True)
+
+    df_path = Path(os.getcwd(), 'input', 'final_short_dataset.csv')
+    df = pd.read_csv(df_path).rename(columns={'Google Trends Score': 'trends_score'})
+
+    countries = list(df.Country.unique())
+    languages = list(df.Language.unique())
+
+    anova_model = 'trends_score ~ Group + Language'
+
+    results_dict = {}
+    results_counter = 0
+
+    for country_name in countries:
+        results_dict[country_name] = {}
+        for language in languages:
+            current_analysis = AnovaAnalysis(df, country_name, language, anova_model, image_out_path)
+
+            results = current_analysis.run_analysis()
+            if results:
+                results_dict[country_name][language] = results
+                results_counter += 1
+
+    print(f'Found {results_counter} results.')
+
+    out_path = Path(output_folder, 'results.pickle')
+    with open(out_path, 'w+b') as file:
+        pickle.dump(results_dict, file)
+
+    print(f'File saved as {out_path}')
+
+
+if __name__ == '__main__':
+    anova_main()
--- notebook_utils/ANOVA/anova_models.py
+++ notebook_utils/ANOVA/anova_models.py
@@ -8,22 +8,21 @@ import statsmodels.api as sm
 from bioinfokit.analys import stat
 from statsmodels.formula.api import ols
 
+from pathlib import Path
+
 warnings.filterwarnings('ignore')
 pd.set_option('display.expand_frame_repr', False)
 
-ANOVA_MODEL = 'search_interest ~ group'
 
-
-def perform_one_way_anova(dataframe):
-    global ANOVA_MODEL
-    model = ols(ANOVA_MODEL, data=dataframe).fit()
+def perform_one_way_anova(dataframe, anova_model):
+    model = ols(anova_model, data=dataframe).fit()
     return sm.stats.anova_lm(model, typ=2)
 
 
-def run_tukey_test(res, dataframe, column_name):
-    res.tukey_hsd(df=dataframe, res_var='search_interest', xfac_var=column_name, anova_model=ANOVA_MODEL)
-    return res.tukey_summary, res.tukey_hsd(df=dataframe, res_var='search_interest', xfac_var=column_name,
-                                            anova_model=ANOVA_MODEL)
+def run_tukey_test(res, dataframe, column_name, res_var, anova_model):
+    res.tukey_hsd(df=dataframe, res_var=res_var, xfac_var=column_name, anova_model=anova_model)
+    return res.tukey_summary, res.tukey_hsd(df=dataframe, res_var=res_var, xfac_var=column_name,
+                                            anova_model=anova_model)
 
 
 def make_qq_plot(res, plot_name):
@@ -51,58 +50,49 @@ def test_shapiro_wilk(res):
 
 
 class AnovaAnalysis:
-    def __init__(self, df_csv, country, anova_model=ANOVA_MODEL):
+    def __init__(self, df: pd.DataFrame, country: str, language: str, anova_model, image_out_path: Path):
         self.res = stat()
-        self.dataframe: pd.DataFrame = self.load_and_process_df(df_csv)
-        self.dataframe = self.dataframe[self.dataframe.country == country]
-        try:
-            self.name = self.dataframe.iloc[1].group_language
-        except IndexError:
-            print('Error: No results found. Cannot run analysis.')
-            self.name = 'Unnamed Analysis'
-        else:
-            self.anova_model = anova_model
-            self.country = country
+        self.dataframe: pd.DataFrame = df
+        self.dataframe = self.dataframe[self.dataframe.Country == country]
 
-    def load_and_process_df(self, csv_path):
-        df = pd.read_csv(csv_path)
-        return df[df['group_value'] == True].drop('Unnamed: 0', axis=1).drop('group_value', axis=1)[
-            ['title', 'country', 'available', 'original_language', 'group', 'group_language', 'search_interest']]
+        self.anova_model = anova_model
+        self.country = country
+        self.language = language
+
+        self.image_out_path = image_out_path
 
     def run_analysis(self):
-        if self.name != 'Unnamed Analysis':
-            print(f'Running Analysis: {self.name} in {self.country}\n\n')
-            anova_df = self.perform_one_way_anova()
+        print(f'Running Analysis: {self.language} in {self.country}\n\n')
+        anova_df = self.perform_one_way_anova()
 
-            print(anova_df)
-            dfi.export(anova_df, f'ANOVA_{self.name}_{self.country}_{self.anova_model}.png')
-            if anova_df.iloc[0]['PR(>F)'] < .05:
-                tukey_test, tukey_obj = self.run_tukey_test('group')
-                print(tukey_test)
-                dfi.export(tukey_test, f'TUKEY_{self.name}_{self.country}.png')
-                shapiro = self.test_shapiro_wilk()
-                print(shapiro)
-            else:
-                print('\nNot significant, skipping tukey test')
+        print(anova_df)
+        dfi.export(anova_df, Path(self.image_out_path, f'ANOVA_{self.language}_{self.country}.png'))
+        if anova_df.iloc[0]['PR(>F)'] < .05:
+            tukey_test, tukey_obj = self.run_tukey_test('Group')
+            print(tukey_test)
+            dfi.export(tukey_test, Path(self.image_out_path, f'TUKEY_{self.language}_{self.country}.png'))
+            shapiro = self.test_shapiro_wilk()
+            print(shapiro)
+            return tukey_test, shapiro
         else:
-            print('\n\nError loading data; skipping analysis\n\n')
+            print('\nNot significant, skipping tukey test')
 
     def perform_one_way_anova(self):
-        print(f'One-Way ANOVA: {self.name}')
-        return perform_one_way_anova(self.dataframe)
+        print(f'One-Way ANOVA: {self.language}')
+        return perform_one_way_anova(self.dataframe, self.anova_model)
 
     def make_qq_plot(self):
-        return make_qq_plot(self.res, self.name)
+        return make_qq_plot(self.res, self.language)
 
     def make_qq_hist(self):
-        return make_qq_hist(self.res, self.name)
+        return make_qq_hist(self.res, self.language)
 
     def test_shapiro_wilk(self):
-        print(f'\n\nShapiro-Wilk Test: {self.name}')
+        print(f'\n\nShapiro-Wilk Test: {self.language}')
         w, pvalue = stats.shapiro(self.res.anova_model_out.resid)
         return (f'W: {w}; P-Value: {pvalue}')
 
     def run_tukey_test(self, column_name):
-        print(f'\n\nTukey Test: {self.name} [{column_name}]')
-        return run_tukey_test(self.res, self.dataframe, column_name)
+        print(f'\n\nTukey Test: {self.language} [{column_name}]')
+        return run_tukey_test(self.res, self.dataframe, column_name, self.anova_model.split(' ~ ')[0], self.anova_model)
 
